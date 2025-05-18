7f9317c3821b411180eec0e5a4c965715ec4250d
imran <mohammedimranofficial98@gmail.com>
2025-05-18 11:10:46 +0530

    SpringApp

---
+++ src/test/java/com/imran/SpringSecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,14 @@
+package com.imran.SpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
